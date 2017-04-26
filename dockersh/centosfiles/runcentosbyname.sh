03329e7ff712bb958e19e5d1858a5cc26484b664
changlizhi <changlizhi@aliyun.com>
2017-04-26 16:25:30 +0800

    pre out systemd

--- dockersh/centosfiles/runcentosbyname.sh
+++ dockersh/centosfiles/runcentosbyname.sh
@@ -4,5 +4,5 @@ IMAGE_VERSION=v1
 IMAGE_NAME=centos_${name}
 IMAGE=${REGISTRY_IP}/${IMAGE_NAME}:${IMAGE_VERSION}
 
-docker build -t ${IMAGE} ./${IMAGE_NAME}/
-docker run -ti --cap-add=SYS_ADMIN -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup ${IMAGE} /usr/sbin/init
+docker build --rm -t ${IMAGE} ./${IMAGE_NAME}/
+#docker run -ti --cap-add=SYS_ADMIN -e "container=docker" -v /sys/fs/cgroup:/sys/fs/cgroup ${IMAGE} /usr/sbin/init
